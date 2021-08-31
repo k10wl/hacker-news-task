3eb958ed6fdc14bd479b9322499b243315a59e19
k10wl <72667957+k10wl@users.noreply.github.com>
2021-08-31 11:23:42 +0300

    added babel and configured webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -14,4 +14,16 @@ module.exports = {
       filename: "index.html"
     })
   ],
+  module: {
+    rules: [{
+      test: /\.js/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"]
+        }
+      }
+    }]
+  }
 };
