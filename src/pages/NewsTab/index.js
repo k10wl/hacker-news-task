bc44b7f1569f4cf126faf49ec50074f932ce524e
k10wl <72667957+k10wl@users.noreply.github.com>
2021-08-31 16:54:51 +0300

    created new component witch will contain tables

---
+++ src/pages/NewsTab/index.js
@@ -0,0 +1,53 @@
+import React from "react";
+import usePageFetch from "../../customHooks/usePageFetch";
+
+function NewsTable() {
+  const [ appData, setAppData ] = React.useState([]);
+  const [ pageNumber, setPageNumber ] = React.useState(1);
+  const pageData = usePageFetch(pageNumber);
+  const { news, loading, error, errorText } = pageData;
+  const observer = React.useRef(null);
+  const lastElementRef = React.useCallback((node) => {
+    if (loading) {
+      return
+    }
+    if (observer.current) {
+      observer.current.disconnect();
+    }
+    observer.current = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        setPageNumber(prevPageNumber => prevPageNumber + 1)
+      }
+    })
+    if (node) {
+      observer.current.observe(node)
+    }
+  }, [loading]);
+  React.useEffect(() => {
+    if (!loading && !error) {
+      setAppData(news);
+    }
+  }, [loading, news, error, pageNumber])
+  function sortByDate() {
+    const sorted = appData.sort((a, b) => b.time - a.time);
+    setAppData([...sorted]);
+  }
+  function sortByName(e) {
+    const sorted = appData.sort((a, b) => a[e.target.id].localeCompare(b[e.target.id]));
+    setAppData([...sorted]);
+  }
+  return (
+    <div>
+      <NewsTable
+        sortByName={sortByName}
+        appData={appData}
+        sortByDate={sortByDate}
+        lastElementRef={lastElementRef}
+      />
+      {loading && !error && <p>Loading...</p>}
+      {error && <p>Error: {errorText}</p>}
+    </div>
+  );
+}
+
+export default NewsTable;
