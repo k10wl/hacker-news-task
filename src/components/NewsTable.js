def01c58d7a0779e2f25bd251f7eeeaa0e77171a
k10wl <72667957+k10wl@users.noreply.github.com>
2021-09-01 17:34:54 +0300

    styled table. made it adaptive and responsive

--- src/components/NewsTable.js
+++ src/components/NewsTable.js
@@ -1,39 +1,54 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import "./stylesheet.css";
 
 function NewsTable({sortByDate, sortByName, appData, lastElementRef}) {
+  const mobileClient =
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEmobileClient|Opera Mini/i.test(navigator.userAgent);
+  const history = useHistory();
   return (
-    <table border="1px">
-      <thead>
-      <tr>
-        <td id="time" onClick={sortByDate}>Time added</td>
-        <td id="title" onClick={sortByName}>Title</td>
-        <td id="domain" onClick={sortByName}>Domain</td>
-      </tr>
-      </thead>
-      <tbody>
-      {appData.map((el, i) => {
-        const date = new Date(el.time);
-        const year = date.getFullYear();
-        const month = date.getMonth() + 1 > 9 ? date.getMonth() + 1 : `0${date.getMonth() + 1}`;
-        const day = date.getDate() > 9 ? date.getDate() : `0${date.getDate()}`;
-        const displayDate = [day, month, year].join("/");
-        return (
-          <tr
-            key={ el.id }
-            onClick={() => console.log(el)}
-            ref={i === appData.length - 1 ? lastElementRef : null}
-          >
-            <td>{ displayDate }</td>
-            <td>
-              <Link to={`comments/id${el.id}`}> {el.title} </Link>
-            </td>
-            <td>{ el.domain }</td>
-          </tr>
-        )
-      })}
-      </tbody>
-    </table>
+    <>
+      <table className={mobileClient ? "mobileClient" : undefined}>
+        <thead>
+        <tr>
+          { !mobileClient && <th id="time" onClick={ sortByDate }>Date</th> }
+          <th id="title" onClick={sortByName}>Title</th>
+          { !mobileClient && <th id="domain" className="domain" onClick={ sortByName }>Domain</th> }
+        </tr>
+        </thead>
+        <tbody>
+        {appData.map((el, i) => {
+          const date = new Date(el.time);
+          const year = date.getFullYear();
+          const month = date.getMonth() + 1 > 9 ? date.getMonth() + 1 : `0${date.getMonth() + 1}`;
+          const day = date.getDate() > 9 ? date.getDate() : `0${date.getDate()}`;
+          const displayDate = [day, month, year].join("/");
+          return (
+            <tr
+              key={ el.id }
+              onClick={() => console.log(el)}
+              ref={i === appData.length - 1 ? lastElementRef : null}
+            >
+              { !mobileClient && <td>{ displayDate }</td> }
+              <td className="cell" onClick={() => {
+                history.push(`comments/id${el.id}`);
+              }}>
+                <span> {el.title} </span>
+              </td>
+              { !mobileClient && <td className="domain">{ el.domain }</td>  }
+            </tr>
+          )
+        })}
+        </tbody>
+      </table>
+      <button
+        style={{ display: mobileClient ? "block" : "none" }}
+        className="sort_by_date"
+        onClick={sortByDate}
+      >
+        Sort by date
+      </button>
+    </>
   );
 }
 
