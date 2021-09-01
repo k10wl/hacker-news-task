b79c8cead1f900124106d5408ea4412f51510115
k10wl <72667957+k10wl@users.noreply.github.com>
2021-09-01 15:46:34 +0300

    added css loader

--- webpack.config.js
+++ webpack.config.js
@@ -22,15 +22,21 @@ module.exports = {
     })
   ],
   module: {
-    rules: [{
-      test: /\.js/,
-      exclude: /node_modules/,
-      use: {
-        loader: "babel-loader",
-        options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"]
+    rules: [
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
         }
-      }
-    }]
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ]
   },
 };
